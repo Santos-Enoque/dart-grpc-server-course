7337f68b9b6f1e82efd254ac1d1510a56760e86a
santos-enoque <santosenoque.ss@gmail.com>
2021-10-07 10:57:04 +0200

    helper class

---
+++ lib/src/helper/methods.dart
@@ -0,0 +1,26 @@
+import 'package:dart_grpc_server/dart_grpc_server.dart';
+
+class HelperMethods {
+
+  Category getCategoryFromMap(Map category) {
+    var _idTag = 1;
+    var _nameTag = 2;
+    int _id = category['id'];
+    String _name = category['name'];
+    return Category.fromJson('{"$_idTag": $_id, "$_nameTag": "$_name"}');
+  }
+
+  Item getItemFromMap(Map item) {
+    var _idTag = 1;
+    var _nameTag = 2;
+    var _categoryTag = 3;
+    int _id = item['id'];
+    String _name = item['name'];
+    int _categoryId = item['categoryId'];
+    return Item.fromJson(
+        '{"$_idTag": $_id, "$_nameTag": "$_name", "$_categoryTag": $_categoryId}');
+  }
+
+}
+
+final helper = HelperMethods();
